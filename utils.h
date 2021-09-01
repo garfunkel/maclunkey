4dd78c44419d6eb9ea896c55a5edce43c9b7e9fd
Simon Allen <simon@simonallen.org>
2021-09-02 05:50:15 +1000

    macro improvements

--- utils.h
+++ utils.h
@@ -11,6 +11,11 @@
 #define ENV_HOME "HOME"
 #define CONFIG_PATH ".config/" APP_NAME "/" APP_NAME ".config"
 
+#define LOG_LEVEL_INFO "INFO"
+#define LOG_LEVEL_WARNING "WARNING"
+#define LOG_LEVEL_ERROR "ERROR"
+#define LOG_LEVEL_FATAL "FATAL"
+
 #define ERR_TABLE(ERR)                       \
 	ERR(ERROR_NETWORK, "Network error")      \
 	ERR(ERROR_TERMINAL, "Terminal error")    \
@@ -20,10 +25,9 @@
 	ERR(ERROR_OS, "OS-level error")          \
 	ERR(ERROR_UNKNOWN, "Unknown error")
 
-#define ERR_ID(id, string) id,
-
 enum ErrId
 {
+#define ERR_ID(id, string) id,
 	ERR_TABLE(ERR_ID)
 #undef ERR_ID
 };
@@ -49,20 +53,22 @@ const char *error_to_string(const enum ErrId id);
 	free(__log_string);                        \
 	__log_string = NULL
 
-#define log_fatal(id, err)         \
-	log_error_x("FATAL", id, err); \
+#define log_fatal(id, err)                 \
+	log_error_x(LOG_LEVEL_FATAL, id, err); \
 	exit(id)
 
-#define log_fatalf(id, fmt, ...) log_error_xf("FATAL", id, fmt, __VA_ARGS__)
+#define log_fatalf(id, fmt, ...)                         \
+	log_error_xf(LOG_LEVEL_FATAL, id, fmt, __VA_ARGS__); \
+	exit(id)
 
-#define log_error(id, err) log_error_x("ERROR", id, err)
-#define log_errorf(id, fmt, ...) log_error_xf("ERROR", id, fmt, __VA_ARGS__)
+#define log_error(id, err) log_error_x(LOG_LEVEL_ERROR, id, err)
+#define log_errorf(id, fmt, ...) log_error_xf(LOG_LEVEL_ERROR, id, fmt, __VA_ARGS__)
 
 #ifdef DEBUG
-	#define log_warning(msg) log_x("WARNING", msg)
-	#define log_warningf(fmt, ...) log_xf("WARNING", fmt, __VA_ARGS__)
-	#define log_info(msg) log_x("INFO", msg)
-	#define log_infof(fmt, ...) log_xf("INFO", fmt, __VA_ARGS__)
+	#define log_warning(msg) log_x(LOG_LEVEL_WARNING, msg)
+	#define log_warningf(fmt, ...) log_xf(LOG_LEVEL_WARNING, fmt, __VA_ARGS__)
+	#define log_info(msg) log_x(LOG_LEVEL_INFO, msg)
+	#define log_infof(fmt, ...) log_xf(LOG_LEVEL_INFO, fmt, __VA_ARGS__)
 #else
 	#define log_warning(msg)
 	#define log_warningf(fmt, ...)
